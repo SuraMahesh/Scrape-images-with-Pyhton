c5d608e62461c7eaa5afeee016b82555236484ca
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-11-13 22:51:33 +0530

    create folder & scraping photos

--- ikman.py
+++ ikman.py
@@ -3,18 +3,23 @@ from bs4 import BeautifulSoup
 import os
 
 
-url = 'https://ikman.lk/en/ads/sri-lanka/property?sort=relevance&buy_now=0&urgent=0&query=colombo&page=1';
+def photos(url, folder):
+    try:
+        os.mkdir(os.path.join(os.getcwd(), folder))
+    except:
+        pass 
+    os.chdir(os.path.join(os.getcwd(), folder)) 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    Img = soup.find_all('img')
 
-r = requests.get(url)
+    for image in Img:
+        name = image['alt']
+        link = image['src']
+        with open(name.replace('', '-').replace('/', '').replace('|', '') + '.jpg', 'wb') as f:
+            im = requests.get(link)
+            f.write(im.content)
+            print('Writing', name)
 
-soup = BeautifulSoup(r.text, 'html.parser')
-
-Img = soup.find_all('img')
-
-for image in Img:
-    name = image['alt']
-    link = image['src']
-    with open(name.replace('', '-').replace('/', '').replace('|', '') + '.jpg', 'wb') as f:
-        im = requests.get(link)
-        f.write(im.content)
+photos('https://ikman.lk/en/ads/sri-lanka/property?sort=relevance&buy_now=0&urgent=0&query=colombo&page=1', 'ikman')        
 
